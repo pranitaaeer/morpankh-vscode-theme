43286b11729eaeece7f5d692ef2c5a9f1f773025
pranitaaeer <aeerpranita@gmail.com>
2026-09-21 01:05:14 +0530

    complete dark theme work

---
+++ script.js
@@ -0,0 +1,11 @@
+function printUser(userName) {
+    console.log("print", userName);
+}
+printUser("pranita")
+
+const showUser = (userName) => {
+    console.log("show",);
+    return;
+}
+let val="pranita"
+showUser("pranita");
